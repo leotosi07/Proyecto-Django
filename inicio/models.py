ee26ced4ed3d813dc490f265e3b7c9659f7f70dc
Leandro Garcia Tosi <leandrotosi07@gmail.com>
2023-04-11 16:04:22 -0300

    Creacion del medelo e incorporacion a la base de dato

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Animal(models.Model):
+    nombre = models.CharField(max_length=20)
+    especie = models.CharField(max_length=20)
+    raza = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField()
+    propietario = models.CharField(max_length=20)
+    contacto_telefonico = models.CharField(max_length=30)
+    fecha_registro = models.DateField(auto_now_add=True)
