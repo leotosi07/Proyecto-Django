4e6fae95d65dabf89de6eaa4752ecc2bda96b513
Leandro Garcia Tosi <leandrotosi07@gmail.com>
2023-04-30 12:02:12 -0300

    Pequenios arreglos

--- inicio/models.py
+++ inicio/models.py
@@ -7,6 +7,4 @@ class Mascota(models.Model):
     especie = models.CharField(max_length=20)
     raza = models.CharField(max_length=20)
     fecha_nacimiento = models.DateField()
-    propietario = models.CharField(max_length=20)
-    contacto_telefonico = models.CharField(max_length=30)
     fecha_registro = models.DateField(auto_now_add=True)
